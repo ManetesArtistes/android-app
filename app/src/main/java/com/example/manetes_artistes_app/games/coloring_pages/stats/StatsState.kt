a36727255e2ea0f407bcab61cdc0257e8f8ce8bf
j <anonymosmail@gmail.com>
2024-11-22 16:52:39 +0100

    add update score to ftp

--- app/src/main/java/com/example/manetes_artistes_app/games/coloring_pages/stats/StatsState.kt
+++ app/src/main/java/com/example/manetes_artistes_app/games/coloring_pages/stats/StatsState.kt
@@ -31,17 +31,17 @@ object StatsState {
         ftpClient.uploadFileToFtp(file, "/json/")
     }
 
-    fun addScore(score: Int, centerId: Int, groupId: Int, stickerId: Int) {
-        val center = centers.find { it.center_id == centerId } ?: Center(centerId).apply { centers.add(this) }
+    fun addScore(score: Int,user: User, context: Context) {
+        val center = centers.find { it.center_id == user.centerId } ?: Center(user.centerId).apply { centers.add(this) }
 
-        val group = center.groups.find { it.group_id == groupId } ?: Group(groupId).apply { center.groups.add(this) }
+        val group = center.groups.find { it.group_id == user.groupId } ?: Group(user.groupId).apply { center.groups.add(this) }
 
-        val student = group.students.find { it.student_id == stickerId } ?: Student(stickerId, Stat(
+        val student = group.students.find { it.student_id == user.stickerId } ?: Student(user.stickerId, Stat(
             mutableListOf(), mutableListOf()
         )).apply { group.students.add(this) }
 
         student.stats.score += score
-//        uploadStatsOnFtp()
+        uploadStatsOnFtp(context)
     }
 
 
